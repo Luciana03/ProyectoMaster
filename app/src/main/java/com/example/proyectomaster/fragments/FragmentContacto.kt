b0f60a3d9f9753fc8e2c4a45b315edd6841f4c02
Luciana03 <lucianalowden@gmail.com>
2020-08-13 19:56:23 -0300

    .

--- app/src/main/java/com/example/proyectomaster/fragments/FragmentContacto.kt
+++ app/src/main/java/com/example/proyectomaster/fragments/FragmentContacto.kt
@@ -25,6 +25,8 @@ class FragmentContacto : Fragment() {
     lateinit var  imageFace: ImageView
     lateinit var  imageInternet: ImageView
     lateinit var  imageInsta: ImageView
+    lateinit var  imageYoutube: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +38,7 @@ class FragmentContacto : Fragment() {
         imageInsta = v.findViewById(R.id.imageInsta)
         imageTw = v.findViewById(R.id.imageTw)
         imageInternet = v.findViewById(R.id.imageInternet)
+        imageInternet = v.findViewById(R.id.imageYoutube)
         txtAboutUs = v.findViewById(R.id.txtAboutUs)
 
         return v
@@ -50,35 +53,35 @@ class FragmentContacto : Fragment() {
                 " - Más info: La Fundación Cardiológica Argentina es una entidad de bien público no gubernamental, sin fines de lucro que se encuentra constituida por profesionales de diferentes especialidades que trabajan en forma honoraria y conforman así el Comité Ejecutivo. Tiene presencia en todo el país por medio de los Distritos Regionales de la Sociedad Argentina de Cardiología. Esta afiliada a la Federación Mundial del Corazón (World Heart Federation) y a la Fundación Interamericana del Corazón (Interamerican Heart Foundation).\n" +
                 "\n" +
                 "En este contexto desarrolla sus actividades que básicamente se dividen en 3 grandes áreas: Investigación, Comunidad y Educación."
-        Glide.with(this).load("http://www.fundacioncardiologica.org/images/FCA-Logo.png").into(imageLogo)
 
-        ///////////////////////////////
-        Glide.with(this).load("https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-2-1.png").into(imageFace);
         imageFace.setOnClickListener {
             val uri2: Uri = Uri.parse("https://es-la.facebook.com/FundacionCardiologicaArgentina/")
             val intent2 = Intent(Intent.ACTION_VIEW, uri2)
             startActivity(intent2)
         }
-        ///////////////////////////////
-        Glide.with(this).load("https://w7.pngwing.com/pngs/16/46/png-transparent-made-in-kings-heath-instagram-facebook-female-graphy-instagram-logo-instagram-icon-text-trademark-magenta.png").into(imageInsta);
+
         imageInsta.setOnClickListener {
             val uri3: Uri = Uri.parse("https://www.instagram.com/fundacioncardiologica/?hl=es-la")
             val intent3 = Intent(Intent.ACTION_VIEW, uri3)
             startActivity(intent3)
         }
-        ///////////////////////////////
-        Glide.with(this).load("https://w7.pngwing.com/pngs/549/715/png-transparent-web-development-logo-website-web-design-symmetry-internet.png").into(imageInternet);
+
         imageInternet.setOnClickListener {
             val uri4: Uri = Uri.parse("http://www.fundacioncardiologica.org/")
             val intent4 = Intent(Intent.ACTION_VIEW, uri4)
             startActivity(intent4)
         }
-        ///////////////////////////////
-        Glide.with(this).load("https://i1.wp.com/www.vectorico.com/wp-content/uploads/2018/02/Twitter-Logo.png?fit=737%2C600").into(imageTw);
+
         imageTw.setOnClickListener {
             val uri: Uri = Uri.parse("https://twitter.com/f_cardiologica?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
+
+        /*imageYoutube.setOnClickListener {
+            val uri: Uri = Uri.parse("https://www.youtube.com/user/fundcardiologica")
+            val intent = Intent(Intent.ACTION_VIEW, uri5)
+            startActivity(intent)
+        }*/
     }
 }
